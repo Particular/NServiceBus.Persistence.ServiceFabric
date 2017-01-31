0ef9c6f14a66eba777b6cdba0e8e9bc4045e67b2
Szymon Kulec <scooletz@gmail.com>
2017-01-31 16:20:30 +0100

    Initial dummy test

---
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/DummyTest.cs
@@ -0,0 +1,12 @@
+﻿namespace NServiceBus.Persistence.ServiceFabric.Tests
+{
+    using NUnit.Framework;
+
+    public class DummyTest
+    {
+        [Test(Description = "Just to satisfy build")]
+        public void Test()
+        {
+        }
+    }
+}

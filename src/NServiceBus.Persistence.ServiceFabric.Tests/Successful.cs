432068b406a46f74f88deb699a3a04ce536d8d75
danielmarbach <daniel.marbach@openplace.net>
2017-03-03 22:23:26 +0100

    Test with assert.True

---
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/Successful.cs
@@ -0,0 +1,14 @@
+namespace NServiceBus.Persistence.ServiceFabric.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Successful
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}

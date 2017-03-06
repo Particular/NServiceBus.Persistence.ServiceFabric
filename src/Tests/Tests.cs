ad8cf58ec63d91af1680bfa122c0ef64236df662
danielmarbach <daniel.marbach@openplace.net>
2017-03-06 15:53:03 +0100

    Rename applications to apply conventions

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -6,10 +6,10 @@ namespace Tests
     {
         // TODO: a few of these settings could be derived by convention
         public static Guid ImageStorePath { get; set; } = new Guid("2DA66BEE-2747-4185-9E5F-3A4951EA074A");
-        public static string ProjectName { get; set; } = "NServiceBus.Persistence.ServiceFabric.TestApplication";
-        public static string ApplicationTypeName { get; set; } = "PersistenceTestsType";
+        public static string ProjectName { get; set; } = "NServiceBus.Persistence.ServiceFabric.TestsApplication";
+        public static string ApplicationTypeName { get; set; } = "TestsType";
         public static Version ApplicationTypeVersion { get; set; } = new Version(1, 0, 0);
-        public static Uri ApplicationName { get; set; } = new Uri("fabric:/PersistenceTests");
-        public static Uri ServiceUri { get; set; } = new Uri("fabric:/PersistenceTests/Tests");
+        public static Uri ApplicationName { get; set; } = new Uri("fabric:/Tests");
+        public static Uri ServiceUri { get; set; } = new Uri("fabric:/Tests/Tests");
     }
 }

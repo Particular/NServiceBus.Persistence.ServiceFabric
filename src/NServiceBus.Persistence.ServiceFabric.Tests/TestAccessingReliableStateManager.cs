5f25e3125dfb62bf3c0e3035fcdc9dcfd3de1fa7
danielmarbach <daniel.marbach@openplace.net>
2017-03-06 15:37:28 +0100

    Enable state manager sample tests

--- src/NServiceBus.Persistence.ServiceFabric.Tests/TestAccessingReliableStateManager.cs
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/TestAccessingReliableStateManager.cs
@@ -1,33 +1,33 @@
-//namespace NServiceBus.Persistence.ServiceFabric.Tests
-//{
-//    using System;
-//    using System.Threading.Tasks;
-//    using Microsoft.ServiceFabric.Data;
-//    using Microsoft.ServiceFabric.Data.Collections;
-//    using NUnit.Framework;
-//    using TestRunner;
+namespace NServiceBus.Persistence.ServiceFabric.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Data;
+    using Microsoft.ServiceFabric.Data.Collections;
+    using NUnit.Framework;
+    using TestRunner;
 
-//    [TestFixture]
-//    public class TestAccessingReliableStateManager : INeed<IReliableStateManager>
-//    {
-//        IReliableStateManager stateManager;
+    [TestFixture]
+    public class TestAccessingReliableStateManager : INeed<IReliableStateManager>
+    {
+        IReliableStateManager stateManager;
 
-//        [Test]
-//        public async Task SomeTest()
-//        {
-//            var state = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("state", TimeSpan.FromSeconds(30)).ConfigureAwait(false);
-//            Assert.AreEqual("urn:state", state.Name.ToString());
-//        }
+        [Test]
+        public async Task SomeTest()
+        {
+            var state = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("state", TimeSpan.FromSeconds(30)).ConfigureAwait(false);
+            Assert.AreEqual("urn:state", state.Name.ToString());
+        }
 
-//        [TearDown]
-//        public void TearDown()
-//        {
+        [TearDown]
+        public void TearDown()
+        {
 
-//        }
+        }
 
-//        public void Need(IReliableStateManager dependency)
-//        {
-//            stateManager = dependency;
-//        }
-//    }
-//}
+        public void Need(IReliableStateManager dependency)
+        {
+            stateManager = dependency;
+        }
+    }
+}
--- src/NServiceBus.Persistence.ServiceFabric.Tests/TestAccessingStatefulContext.cs
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/TestAccessingStatefulContext.cs
@@ -1,36 +1,36 @@
-﻿//namespace NServiceBus.Persistence.ServiceFabric.Tests
-//{
-//    using System;
-//    using System.Fabric;
-//    using NUnit.Framework;
-//    using TestRunner;
+﻿namespace NServiceBus.Persistence.ServiceFabric.Tests
+{
+    using System;
+    using System.Fabric;
+    using NUnit.Framework;
+    using TestRunner;
 
-//    [TestFixture]
-//    public class TestAccessingStatefulContext : INeed<StatefulServiceContext>
-//    {
-//        public void Need(StatefulServiceContext dependency)
-//        {
-//            statefulServiceContext = dependency;
-//        }
+    [TestFixture]
+    public class TestAccessingStatefulContext : INeed<StatefulServiceContext>
+    {
+        public void Need(StatefulServiceContext dependency)
+        {
+            statefulServiceContext = dependency;
+        }
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//        }
+        [SetUp]
+        public void SetUp()
+        {
+        }
 
-//        [Test]
-//        public void SomeTest()
-//        {
-//            Console.WriteLine(statefulServiceContext.ServiceName);
-//            Console.WriteLine(statefulServiceContext.ReplicaId);
-//            Assert.AreEqual("TestsType", statefulServiceContext.ServiceTypeName);
-//        }
+        [Test]
+        public void SomeTest()
+        {
+            Console.WriteLine(statefulServiceContext.ServiceName);
+            Console.WriteLine(statefulServiceContext.ReplicaId);
+            Assert.AreEqual("TestsType", statefulServiceContext.ServiceTypeName);
+        }
 
-//        [TearDown]
-//        public void TearDown()
-//        {
-//        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
 
-//        StatefulServiceContext statefulServiceContext;
-//    }
-//}
+        StatefulServiceContext statefulServiceContext;
+    }
+}

e6532ef65096dafa97553fd086c51914c15ee52a
danielmarbach <daniel.marbach@openplace.net>
2017-03-08 12:32:01 +0100

    Bringing in the acceptance tests

---
+++ src/NServiceBus.Persistence.ServiceFabric.AcceptanceTests/App_Packages/NSB.AcceptanceTests.6.1.3/Routing/MessageDrivenSubscriptions/When_subscribing_to_multiple_publishers.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing.MessageDrivenSubscriptions
+{
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+
+    public class When_subscribing_to_multiple_publishers : NServiceBusAcceptanceTest
+    {
+        class Context : ScenarioContext
+        {
+            public bool SubscribedToPublisher1 { get; set; }
+            public bool SubscribedToPublisher2 { get; set; }
+        }
+
+        class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.DisableFeature<AutoSubscribe>();
+                }, metadata =>
+                {
+                    metadata.RegisterPublisherFor<SomeEvent>("Publisher1");
+                    metadata.RegisterPublisherFor<SomeEvent>("Publisher2");
+                });
+            }
+        }
+
+        class Publisher : EndpointConfigurationBuilder
+        {
+            public Publisher()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        public class SomeEvent : IEvent
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.Persistence.ServiceFabric.AcceptanceTests/ConfigureScenariosForAzureServiceBusTransport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+public class ConfigureScenariosForAzureServiceBusTransport : IConfigureSupportedScenariosForTestExecution
+{
+    public IEnumerable<Type> UnsupportedScenarioDescriptorTypes { get; } = new[]
+    {
+        typeof(AllDtcTransports),
+        typeof(AllTransportsWithMessageDrivenPubSub),
+        typeof(AllTransportsWithoutNativeDeferral),
+        typeof(AllNativeMultiQueueTransactionTransports),
+    };
+}

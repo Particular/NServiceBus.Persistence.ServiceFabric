86f00fbe1a8071176cec7808bac62d2f40a519d9
danielmarbach <daniel.marbach@openplace.net>
2017-03-09 11:06:27 +0100

    Basic infrastructure for component tests

---
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/ComponentTests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Persistence.ComponentTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Extensibility;
+    using Gateway.Deduplication;
+    using Outbox;
+    using Sagas;
+    using Timeout.Core;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    public partial class PersistenceTestsConfiguration : IPersistenceTestsConfiguration
+    {
+        public Func<ContextBag> GetContextBagForTimeoutPersister { get; set; } = () => new ContextBag();
+
+        public Func<Action, ContextBag> Func;
+    }
+
+    public interface IPersistenceTestsConfiguration
+    {
+        ISagaPersister SagaStorage { get; }
+
+        ISynchronizedStorage SynchronizedStorage { get; }
+
+        ISubscriptionStorage SubscriptionStorage { get; }
+
+        IPersistTimeouts TimeoutStorage { get; }
+
+        IQueryTimeouts TimeoutQuery { get; }
+
+        IOutboxStorage OutboxStorage { get; }
+
+        IDeduplicateMessages GatewayStorage { get; }
+
+        Task Configure();
+
+        Task Cleanup();
+    }
+}
---
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace NServiceBus.Persistence.ComponentTests
+{
+    using System.Threading.Tasks;
+    using Gateway.Deduplication;
+    using NUnit.Framework;
+    using Outbox;
+    using Sagas;
+    using ServiceFabric;
+    using Timeout.Core;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+    using StatefulService = Microsoft.ServiceFabric.Services.Runtime.StatefulService;
+
+    public partial class PersistenceTestsConfiguration
+    {
+        public PersistenceTestsConfiguration()
+        {
+            var statefulService = (StatefulService) TestContext.CurrentContext.Test.Properties.Get("StatefulService");
+
+            SynchronizedStorage = new SynchronizedStorage(statefulService.StateManager);
+        }
+
+        public ISagaPersister SagaStorage { get; }
+        public ISynchronizedStorage SynchronizedStorage { get; }
+        public ISubscriptionStorage SubscriptionStorage { get; }
+        public IPersistTimeouts TimeoutStorage { get; }
+        public IQueryTimeouts TimeoutQuery { get; }
+        public IOutboxStorage OutboxStorage { get; }
+        public IDeduplicateMessages GatewayStorage { get; }
+
+        public Task Configure()
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task Cleanup()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}

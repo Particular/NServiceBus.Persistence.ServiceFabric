18c04bcdc704a1b76e2cfacc5d85699fd67dd552
danielmarbach <daniel.marbach@openplace.net>
2017-03-15 12:40:48 +0100

    Bringing in more let's see scenarios

--- src/NServiceBus.Persistence.ServiceFabric.Tests/LetsSeeTest.cs
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/LetsSeeTest.cs
@@ -7,6 +7,7 @@
     using Microsoft.ServiceFabric.Data.Collections;
     using NUnit.Framework;
 
+    [TestFixture]
     public class LetsSeeTest : INeed<IReliableStateManager>
     {
         IReliableStateManager stateManager;
@@ -62,6 +63,75 @@
                 Console.WriteLine($"Result t2 {result}");
                 losingTransaction.Dispose();
                 Assert.IsFalse(result, "Expected to fail to update the value, but didn't.");
+
+                transaction = stateManager.CreateTransaction();
+                var conditional = await primary.TryGetValueAsync(transaction, "Key");
+                transaction.Dispose();
+
+                Assert.AreEqual("Value1", conditional.Value);
+            });
+
+            await t2;
+        }
+
+        [Test]
+        [Ignore("is not supported by SF")]
+        public async Task LetsSeeInterleaved()
+        {
+            var primary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("index1", TimeSpan.FromSeconds(5));
+            var secondary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("index2", TimeSpan.FromSeconds(5));
+
+            var transaction = stateManager.CreateTransaction();
+            await primary.AddOrUpdateAsync(transaction, "Key", "Value", (s, s1) => "Value");
+            await secondary.AddOrUpdateAsync(transaction, "Key", "Key", (s, s1) => "Key");
+            await transaction.CommitAsync();
+            transaction.Dispose();
+
+            var tcs = new TaskCompletionSource<bool>();
+            var startSync = new TaskCompletionSource<bool>();
+
+            var t1 = Task.Run(async () =>
+            {
+                var winningTransaction = stateManager.CreateTransaction();
+                var conditional = await secondary.TryGetValueAsync(winningTransaction, "Key");
+                await primary.TryGetValueAsync(winningTransaction, conditional.Value);
+                await winningTransaction.CommitAsync();
+                winningTransaction.Dispose();
+
+                winningTransaction = stateManager.CreateTransaction();
+                var result = await primary.TryUpdateAsync(winningTransaction, "Key", "Value1", "Value");
+                Console.WriteLine($"Result t1 { result }");
+                startSync.SetResult(true);
+                await tcs.Task;
+
+                await winningTransaction.CommitAsync();
+                winningTransaction.Dispose();
+
+            });
+
+            var t2 = Task.Run(async () =>
+            {
+                await startSync.Task;
+
+                var losingTransaction = stateManager.CreateTransaction();
+                await primary.TryGetValueAsync(losingTransaction, "Key");
+                await losingTransaction.CommitAsync();
+                losingTransaction.Dispose();
+
+                losingTransaction = stateManager.CreateTransaction();
+                var result = await primary.TryUpdateAsync(losingTransaction, "Key", "Value2", "Value");
+                Console.WriteLine($"Result t2 {result}");
+                tcs.SetResult(true);
+                losingTransaction.Dispose();
+                Assert.IsFalse(result, "Expected to fail to update the value, but didn't.");
+
+                await t1;
+
+                transaction = stateManager.CreateTransaction();
+                var conditional = await primary.TryGetValueAsync(transaction, "Key");
+                transaction.Dispose();
+
+                Assert.AreEqual("Value1", conditional.Value);
             });
 
             await t2;
@@ -98,8 +168,57 @@
             losingTransaction = stateManager.CreateTransaction();
             result = await primary.TryUpdateAsync(losingTransaction, "Key", "Value2", "Value");
             Console.WriteLine($"Result t2 {result}");
+            await losingTransaction.CommitAsync();
             losingTransaction.Dispose();
             Assert.IsFalse(result, "Expected to fail to update the value, but didn't.");
+
+            transaction = stateManager.CreateTransaction();
+            conditional = await primary.TryGetValueAsync(transaction, "Key");
+            transaction.Dispose();
+
+            Assert.AreEqual("Value1", conditional.Value);
+        }
+
+        [Test]
+        [Ignore("is not supported by SF")]
+        public async Task LetsSeeWithoutThreadsInterleaved()
+        {
+            var primary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("index1", TimeSpan.FromSeconds(5));
+            var secondary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>("index2", TimeSpan.FromSeconds(5));
+
+            var transaction = stateManager.CreateTransaction();
+            await primary.AddOrUpdateAsync(transaction, "Key", "Value", (s, s1) => "Value");
+            await secondary.AddOrUpdateAsync(transaction, "Key", "Key", (s, s1) => "Key");
+            await transaction.CommitAsync();
+            transaction.Dispose();
+
+            var winningTransaction = stateManager.CreateTransaction();
+            var conditional = await secondary.TryGetValueAsync(winningTransaction, "Key");
+            await primary.TryGetValueAsync(winningTransaction, conditional.Value);
+            winningTransaction.Dispose();
+
+            var losingTransaction = stateManager.CreateTransaction();
+            conditional = await secondary.TryGetValueAsync(winningTransaction, "Key");
+            await primary.TryGetValueAsync(losingTransaction, conditional.Value);
+            losingTransaction.Dispose();
+
+            winningTransaction = stateManager.CreateTransaction();
+            losingTransaction = stateManager.CreateTransaction();
+            var result = await primary.TryUpdateAsync(winningTransaction, "Key", "Value1", "Value");
+            Console.WriteLine($"Result t1 { result }");
+            result = await primary.TryUpdateAsync(losingTransaction, "Key", "Value2", "Value");
+            Console.WriteLine($"Result t2 {result}");
+            await winningTransaction.CommitAsync();
+            winningTransaction.Dispose();
+
+            losingTransaction.Dispose();
+            Assert.IsFalse(result, "Expected to fail to update the value, but didn't.");
+
+            transaction = stateManager.CreateTransaction();
+            conditional = await primary.TryGetValueAsync(transaction, "Key");
+            transaction.Dispose();
+
+            Assert.AreEqual("Value1", conditional.Value);
         }
 
         public void Need(IReliableStateManager dependency)

c2346f482088d3fc21ef8c9cfc64bffc4900519c
danielmarbach <daniel.marbach@openplace.net>
2017-03-15 12:12:45 +0100

    Allowing to enqueue operations on storage session

--- src/NServiceBus.Persistence.ServiceFabric/SynchronizedStorage/StorageSession.cs
+++ src/NServiceBus.Persistence.ServiceFabric/SynchronizedStorage/StorageSession.cs
@@ -1,6 +1,7 @@
 namespace NServiceBus.Persistence.ServiceFabric
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.ServiceFabric.Data;
 
@@ -12,27 +13,43 @@ namespace NServiceBus.Persistence.ServiceFabric
         {
             StateManager = stateManager;
             _transaction = transaction;
+            actions = new List<Func<ITransaction, Task>>();
         }
 
         public IReliableStateManager StateManager { get; }
 
         public ITransaction Transaction => _transaction.Value;
 
+        // this will lead to closure allocations
+        List<Func<ITransaction, Task>> actions;
+
         public void Dispose()
         {
+            actions.Clear();
+
             if (_transaction.IsValueCreated)
             {
                 _transaction.Value.Dispose();
             }
         }
 
-        public Task CompleteAsync()
+        public Task Add(Func<ITransaction, Task> action)
         {
+            actions.Add(action);
+            return TaskEx.CompletedTask;
+        }
+
+        public async Task CompleteAsync()
+        {
+            foreach (var action in actions)
+            {
+                await action(Transaction).ConfigureAwait(false);
+            }
+
             if (_transaction.IsValueCreated)
             {
-                return _transaction.Value.CommitAsync();
+                await _transaction.Value.CommitAsync().ConfigureAwait(false);
             }
-            return TaskEx.CompletedTask;
         }
     }
 }

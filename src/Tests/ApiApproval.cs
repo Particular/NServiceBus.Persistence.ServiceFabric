83ac0534a558c391c0c75ebdaf84f8092282a699
danielmarbach <daniel.marbach@openplace.net>
2017-03-16 13:24:09 +0100

    Approving the API

---
+++ src/Tests/ApiApproval.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using ApprovalTests;
+    using NServiceBus.Persistence.ServiceFabric;
+    using NUnit.Framework;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ApproveNServiceBus()
+        {
+            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(ServiceFabricPersistence).Assembly));
+            Approvals.Verify(publicApi);
+        }
+
+        string Filter(string text)
+        {
+            return string.Join(Environment.NewLine, text.Split(new[]
+                {
+                    Environment.NewLine
+                }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+            );
+        }
+    }
+}

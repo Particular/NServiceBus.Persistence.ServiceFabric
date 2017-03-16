26f97931b96b7d7aebcc08d22543358e1051b80a
danielmarbach <daniel.marbach@openplace.net>
2017-03-16 10:59:55 +0100

    Deleted timeout, subscription and gateway support from in memory

--- src/NServiceBus.Persistence.ServiceFabric/ServiceFabricPersistence.cs
+++ src/NServiceBus.Persistence.ServiceFabric/ServiceFabricPersistence.cs
@@ -1,11 +1,8 @@
 ﻿namespace NServiceBus.Persistence.ServiceFabric
 {
-    using Gateway;
     using Outbox;
     using Persistence;
     using SagaPersister;
-    using SubscriptionStorage;
-    using TimeoutPersister;
     using SettingsExtensions = Features.SettingsExtensions;
 
     /// <summary>
@@ -16,10 +13,7 @@
         internal ServiceFabricPersistence()
         {
             Supports<StorageType.Sagas>(s => SettingsExtensions.EnableFeatureByDefault<SagaPersistenceFeature>(s));
-            Supports<StorageType.Timeouts>(s => SettingsExtensions.EnableFeatureByDefault<ServiceFabricTimeoutPersistence>(s));
-            Supports<StorageType.Subscriptions>(s => SettingsExtensions.EnableFeatureByDefault<ServiceFabricSubscriptionPersistence>(s));
             Supports<StorageType.Outbox>(s => SettingsExtensions.EnableFeatureByDefault<ServiceFabricOutboxPersistence>(s));
-            Supports<StorageType.GatewayDeduplication>(s => SettingsExtensions.EnableFeatureByDefault<ServiceFabricGatewayPersistence>(s));
         }
     }
 }

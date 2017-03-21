7a0e445dcec6699b6374c36c2e744e06ec2c6289
danielmarbach <daniel.marbach@openplace.net>
2017-03-21 16:20:51 +0100

    Unique saga names and moved out SynchronizedStorageSessionExtensions

---
+++ src/NServiceBus.Persistence.ServiceFabric/SagaPersister/SynchronizedStorageSessionExtensions.cs
@@ -0,0 +1,14 @@
+namespace NServiceBus.Persistence.ServiceFabric
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Data.Collections;
+
+    static class SynchronizedStorageSessionExtensions
+    {
+        public static Task<IReliableDictionary<Guid, SagaEntry>> Sagas(this StorageSession session, string collectionName)
+        {
+            return session.StateManager.GetOrAddAsync<IReliableDictionary<Guid, SagaEntry>>(collectionName);
+        }
+    }
+}

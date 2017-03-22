aba209e5a5f773461183647dd3d72ff1d9608ddf
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-03-22 09:06:42 +0100

    typos and minor changes ServiceFabricSagaAttribute

--- src/NServiceBus.Persistence.ServiceFabric/SagaPersister/ServiceFabricSagaAttribute.cs
+++ src/NServiceBus.Persistence.ServiceFabric/SagaPersister/ServiceFabricSagaAttribute.cs
@@ -3,22 +3,22 @@
     using System;
 
     /// <summary>
-    /// Allows to influence the collection name and/or the saga data name when a saga is stored into reliable collections.
+    /// Defines the collection name and/or the saga data name used for storing saga data.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public sealed class ServiceFabricSagaAttribute : Attribute
     {
         /// <summary>
-        /// The saga data name is primarily used for backwards compatibility. By default the saga data type name is used
-        /// to create a determinstic identifier to store the saga into the reliable collection. Renaming a saga data
-        /// will therefore influence the identifier generation. By setting the saga data name to saga data name before
-        /// the renaming the identifier generation will rename stable.
+        /// The saga data name should primarily be used to preserve backwards compatibility. By default, the saga data type
+        /// name is used to create a deterministic identifier to store the saga in the reliable collection. Renaming a saga data
+        /// class name will therefore change the saga data identifier. Setting the saga data name before renaming the
+        /// class will prevent identifier value from changing.
         /// </summary>
         public string SagaDataName;
 
         /// <summary>
-        /// The collection name allows to influence in which reliable collection the saga data will be stored. By default
-        /// each saga data will be stored in a collection named after the saga data type name.
+        /// The reliable collection name in which the saga data will be stored. By default each saga data will be stored
+        /// in a collection named after the saga data type name.
         /// </summary>
         public string CollectionName;
     }

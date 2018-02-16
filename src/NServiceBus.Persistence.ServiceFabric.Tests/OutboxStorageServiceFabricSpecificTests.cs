9945c94935cd302d0331c333123fa6beecfce1a1
danielmarbach <daniel.marbach@openplace.net>
2018-02-16 11:06:19 +0100

    Concurrent insert test

--- src/NServiceBus.Persistence.ServiceFabric.Tests/OutboxStorageServiceFabricSpecificTests.cs
+++ src/NServiceBus.Persistence.ServiceFabric.Tests/OutboxStorageServiceFabricSpecificTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace NServiceBus.Persistence.ComponentTests
 {
     using System;
+    using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using global::TestRunner;
     using Microsoft.ServiceFabric.Data;
@@ -61,6 +63,57 @@
             }
 
             Assert.Null(message);
+        }
+
+        [Test]
+        public async Task Should_clear_dispatched_messages_and_cleanup_queue_after_given_expiry_even_under_high_concurrency()
+        {
+            configuration.RequiresOutboxSupport();
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(60));
+
+            var storage = (OutboxStorage)configuration.OutboxStorage;
+            var ctx = configuration.GetContextBagForOutbox();
+
+            var cleanupTask = Task.Run(async () =>
+            {
+                using (var tx = stateManager.CreateTransaction())
+                {
+                    await storage.Cleanup.GetCountAsync(tx);
+                }
+
+                long cleanupCount = 1;
+                while (!cts.IsCancellationRequested && cleanupCount > 0)
+                {
+                    await configuration.CleanupMessagesOlderThan(DateTimeOffset.UtcNow);
+
+                    using (var tx = stateManager.CreateTransaction())
+                    {
+                        cleanupCount = await storage.Cleanup.GetCountAsync(tx) + await storage.Outbox.GetCountAsync(tx);
+                    }
+                }
+
+                return cleanupCount;
+            }, CancellationToken.None);
+
+            var inserts = Enumerable.Repeat(1, 200).Select(async _ =>
+            {
+                var messageId = Guid.NewGuid().ToString();
+
+                var messageToStore = new OutboxMessage(messageId, new[] {new TransportOperation("x", null, null, null)});
+                using (var transaction = await storage.BeginTransaction(ctx))
+                {
+                    await storage.Store(messageToStore, transaction, ctx);
+
+                    await transaction.Commit();
+                }
+
+                await storage.SetAsDispatched(messageId, configuration.GetContextBagForOutbox());
+            });
+
+            await Task.WhenAll(inserts);
+
+            Assert.AreEqual(0, await cleanupTask);
         }
 
         public void Need(IReliableStateManager dependency)

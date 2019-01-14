54b230622558b2a1efef964a3a4df9c2c15cd875
Brandon Ording <bording@gmail.com>
2019-01-14 15:30:06 -0500

    Switch to Particular.Approvals

--- src/ApiTests/ApiApproval.cs
+++ src/ApiTests/ApiApproval.cs
@@ -1,22 +1,15 @@
-﻿using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using ApprovalTests;
-using ApprovalTests.Reporters;
+﻿using NServiceBus.Persistence.ServiceFabric;
 using NUnit.Framework;
+using Particular.Approvals;
 using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
 {
     [Test]
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    [UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
     public void ApproveServiceFabricPersistence()
     {
-        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Persistence.ServiceFabric.dll");
-        var assembly = Assembly.LoadFile(combine);
-        var publicApi = ApiGenerator.GeneratePublicApi(assembly);
-        Approvals.Verify(publicApi);
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(ServiceFabricPersistence).Assembly);
+        Approver.Verify(publicApi);
     }
 }

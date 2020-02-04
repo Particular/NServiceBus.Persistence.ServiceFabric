edfa901bae71a6fd94f477517710032b8abc988e
mauroservienti <mauro.servienti@gmail.com>
2020-02-04 17:22:32 +0100

    not needed using

--- src/Tests/ComponentTests/IPersistenceTestsConfiguration.cs
+++ src/Tests/ComponentTests/IPersistenceTestsConfiguration.cs
@@ -3,7 +3,6 @@ namespace NServiceBus.Persistence.ComponentTests
 {
     using System;
     using System.Threading.Tasks;
-    using Gateway.Deduplication;
     using Outbox;
     using Sagas;
     using Timeout.Core;

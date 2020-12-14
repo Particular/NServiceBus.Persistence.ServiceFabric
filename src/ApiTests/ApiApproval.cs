990b75753be722db16a9fc9b535f6485bd7264ef
Sean Feldman <feldman.sean@gmail.com>
2020-12-14 11:13:25 -0700

    Fix API Approvals

--- src/ApiTests/ApiApproval.cs
+++ src/ApiTests/ApiApproval.cs
@@ -9,7 +9,10 @@ public class APIApprovals
     [Test]
     public void ApproveServiceFabricPersistence()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(ServiceFabricPersistence).Assembly);
+        var publicApi = typeof(ServiceFabricPersistence).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
         Approver.Verify(publicApi);
     }
 }

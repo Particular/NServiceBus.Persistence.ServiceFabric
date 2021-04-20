43018f269123124bbf0edaa3a99f6c3c23d62b36
Adam Ralph <adam@adamralph.com>
2021-04-20 20:21:39 +0200

    fixup

--- src/ServiceFabricPersistence/Sagas/SynchronizedStorageSessionExtensions.cs
+++ src/ServiceFabricPersistence/Sagas/SynchronizedStorageSessionExtensions.cs
@@ -7,9 +7,7 @@ namespace NServiceBus.Persistence.ServiceFabric
 
     static class SynchronizedStorageSessionExtensions
     {
-#pragma warning disable IDE0060 // Remove unused parameter
         public static Task<IReliableDictionary<Guid, SagaEntry>> Sagas(this StorageSession session, string collectionName, CancellationToken cancellationToken = default)
-#pragma warning restore IDE0060 // Remove unused parameter
         {
             return session.StateManager.GetOrAddAsync<IReliableDictionary<Guid, SagaEntry>>(collectionName, session.TransactionTimeout);
         }
